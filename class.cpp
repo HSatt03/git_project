acbd04070caacef05055cd8dcdda80af693318e3
Hasti <hsattari2006@gmail.com>
2025-02-27 19:32:22 +0330

    seventh

--- class.cpp
+++ class.cpp
@@ -8,9 +8,16 @@ public:
     string name;
     string food;
 };
+class Student
+{
+public:
+    int id;
+    string LastName;
+};
 int main()
 {
     Person P;
+    Student Stu;
     cout << "Enter your name!" << endl;
     getline(cin, P.name);
     cout << "Hello " << P.name << "!" << endl;
@@ -21,5 +28,11 @@ int main()
     cout << "Which food do you like?" << endl;
     getline(cin, P.food);
     cout << "You like " << P.food << " very much!" << endl;
+    cout << "+++++++++++++++++" << "\n";
+    cout << "Enter your lastname: " << endl;
+    getline(cin, Stu.LastName);
+    cout << "Your id: " << endl;
+    cin >> Stu.id;
+    cout << "your lastname is " << Stu.LastName << " and your id is " << Stu.id << endl;
     return 0;
 }
