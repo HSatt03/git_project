dc56fbd601fc8b72eaca03b0bef9f26d13069751
Hasti <hsattari2006@gmail.com>
2025-02-27 17:15:42 +0330

    third

--- class.cpp
+++ class.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+class Person
+{
+public:
+    int age;
+};
 int main()
 {
     string name;
+    Person P;
     cout << "Enter your name!" << endl;
     getline(cin, name);
     cout << "Hello " << name << "!" << endl;
+    cout << "Enter yuor age!" << "\n";
+    cin >> P.age;
+    cout << "You are " << P.age << "years old!" << endl;
     return 0;
 }
